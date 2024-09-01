c12a4323c8921fb9e7af40637d7354a4c1d4ee1f
LucasCoppola <lucascoppola21@gmail.com>
2024-08-31 22:15:44 -0300

    update diff view window position

--- nvim/lua/plugins/diff_view.lua
+++ nvim/lua/plugins/diff_view.lua
@@ -1,1 +1,14 @@
-return { "sindrets/diffview.nvim" }
+return {
+	{
+		"sindrets/diffview.nvim",
+		config = function()
+			require("diffview").setup({
+				file_panel = {
+					win_config = { -- See |diffview-config-win_config|
+						position = "bottom",
+					},
+				},
+			})
+		end,
+	},
+}
