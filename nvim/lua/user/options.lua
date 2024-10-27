5bfe0fb956ec8d40ea680103c126b444b75c8f0a
LucasCoppola <lucascoppola21@gmail.com>
2024-10-27 15:02:01 -0300

    no wrap and no linebreak

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -11,10 +11,12 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 
+-- Disable wrap and linebreak which are enabled on md and txt
+vim.opt.wrap = false
+vim.opt.linebreak = false
+
 -- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
 vim.opt.breakindent = true
-
-vim.opt.linebreak = true
 
 -- Enable incremental searching
 vim.opt.incsearch = true
