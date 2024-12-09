b462f432c0c69d210a31e7ced234910f9b95d0e5
LucasCoppola <lucascoppola21@gmail.com>
2024-12-08 22:53:46 -0300

    add fugitive

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -6,7 +6,6 @@ local xnoremap = require("user.keymap_utils").xnoremap
 local harpoon_ui = require("harpoon.ui")
 local harpoon_mark = require("harpoon.mark")
 local illuminate = require("illuminate")
-local utils = require("user.utils")
 local supermaven = require("supermaven-nvim.api")
 
 local M = {}
@@ -237,26 +236,9 @@ nnoremap("<leader>4", function()
 end, { desc = "Harpoon 4" })
 
 -- Git keymaps --
-nnoremap("gb", ":Gitsigns toggle_current_line_blame<cr>")
-nnoremap("<leader>gf", function()
-	local cmd = {
-		"sort",
-		"-u",
-		"<(git diff --name-only --cached)",
-		"<(git diff --name-only)",
-		"<(git diff --name-only --diff-filter=U)",
-	}
 
-	if not utils.is_git_directory() then
-		vim.notify(
-			"Current project is not a git directory",
-			vim.log.levels.WARN,
-			{ title = "Telescope Git Files", git_command = cmd }
-		)
-	else
-		require("telescope.builtin").git_files()
-	end
-end, { desc = "Search [G]it [F]iles" })
+-- Open git fugitive
+nnoremap("<leader>g", "<cmd>Git<cr>", { silent = false, desc = "Save File" })
 
 -- Telescope keybinds --
 nnoremap("<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
