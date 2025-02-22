b2c53978e814ccb5b23b28226c1bfa54c39ca613
LucasCoppola <lucascoppola21@gmail.com>
2025-02-22 16:53:09 -0300

    update polybar

--- polybar/chnvok/scripts/playerctl-status.sh
+++ polybar/chnvok/scripts/playerctl-status.sh
@@ -3,10 +3,15 @@ if ! playerctl status &>/dev/null; then
     exit 1
 fi
 
-TITLE=$(playerctl metadata title 2>/dev/null)
-TITLE=${TITLE:0:30}
+if ! playerctl -l | grep -q "spotify"; then
+    exit 1
+fi
 
-ARTIST=$(playerctl metadata artist 2>/dev/null)
+playerctl -p spotify metadata &>/dev/null || exit 1
+
+TITLE=$(playerctl -p spotify metadata title 2>/dev/null)
+TITLE=${TITLE:0:30}
+ARTIST=$(playerctl -p spotify metadata artist 2>/dev/null)
 ICON_MUSIC=""
 
 if [ -n "$ARTIST" ]; then
